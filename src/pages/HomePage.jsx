b36a31ab939fa25a2ab3364e75190f203cc3827d
azooz <no-reply@example.com>
2025-04-12 15:43:10 +0300

    Designed the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,16 @@
 export default function HomePage() {
-  return (
-    <span>Hello World</span>
-  )
+  //TODO: Add more things to do on the blog
+  //TODO: Render content based on the user login state
+  return <>
+    <article>
+      <section id="call-to-action">
+        <h2>Interesting articles</h2>
+        <p>On this blog you can find a lot of articles and you can publish your own articles</p>
+        <p>Login or register now to start publishing</p>
+        <div id="call-to-action-buttons">
+          <button>Login</button><button>Register</button>
+        </div>
+      </section>
+    </article>
+  </>
 }
