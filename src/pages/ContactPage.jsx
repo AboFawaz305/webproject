9bbf2fc8628cbcd13d553f2a004e0796eb6f975c
azooz <no-reply@example.com>
2025-04-12 16:22:09 +0300

    Designed Contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,10 @@
 export default function ContactPage() {
-  return (
-    <span>Contact page</span>
-  )
+  return <>
+    <article>
+      <section>
+        <h2>Contact me</h2>
+        <p>If you have something to talk about with me you can <a href="mailto:443102278@student.ksu.edu.sa">send me an email</a> at 443102278@ksu.edu.sa</p>
+      </section>
+    </article>
+  </>
 }
