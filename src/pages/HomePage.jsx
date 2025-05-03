09e0552eed2e39ca0a50013bf1e1bd49fb8018ff
azooz <no-reply@example.com>
2025-05-03 18:26:45 +0300

    UPDATED

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,12 +9,13 @@ export default function HomePage() {
         <h2>Interesting articles</h2>
         <p>On this blog you can find a lot of articles and you can publish your own articles</p>
         {
-          !isLoggedIn &&
-          <> <p>Login or register now to start publishing</p>
-            <div id="call-to-action-buttons">
-              <Link to="/login" className="btn">Login</Link>
-              <Link to="/register" className="btn">Register</Link>
-            </div></>
+          !isLoggedIn ?
+            <> <p>Login or register now to start publishing</p>
+              <div id="call-to-action-buttons">
+                <Link to="/login" className="btn">Login</Link>
+                <Link to="/register" className="btn">Register</Link>
+              </div></> :
+            <span><Link to="/blogs/new">Create a new Article</Link></span>
         }
       </section>
     </article>
